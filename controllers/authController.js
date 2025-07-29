0ec262ec5a73528df7e09cb5ea30ec01a9bdb6fc
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2025-07-29 22:27:38 +0430

    creating login functionlity

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,6 +6,7 @@ const AppError = require("../utils/appError");
 const Organization = require("../models/organization");
 const bcrypt = require("bcryptjs");
 
+
 const generateToken = (id, organizationId) => {
      // eslint-disable-next-line no-undef
     return jwt.sign({ id, organizationId }, process.env.JWT_SECRET, {
@@ -18,14 +19,14 @@ const generateToken = (id, organizationId) => {
 
 exports.register = catchAsynch(async (req, res, next) => {
     
-    const { username, password, email, userTypeId, organizationName, whatsApp } = req.body;
+    const { username, password, email, usertypeId, organizationName, whatsApp } = req.body;
 
     if (!username || !password || !email || !organizationName) {
         return next(new AppError("please provide the required field", 400));
     }
     
 
-    const existingUser = await UserAccount.findOne({ where: email });
+    const existingUser = await UserAccount.findOne({ where: {email} });
 
     if (existingUser) {
         return next(new AppError("the email has already exists", 400));
@@ -38,29 +39,54 @@ exports.register = catchAsynch(async (req, res, next) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // creating new user
-
-    const newUser = await UserAccount.create({
-        username,
-        password: hashedPassword,
-        email,
-        userTypeId,
-        whatsApp,
-        organizationId: organization.id
-    })
-
-    const token = generateToken(newUser.id, organization.id);
-
-    res.status(200).json({
-        status: "success",
-        data: {
-            token
-        }
-    });
+  try {
+    
+      const newUser = await UserAccount.create({
+          username,
+          password: hashedPassword,
+          email,
+          usertypeId,
+          whatsApp,
+          organizationId: organization.id
+        })
+        
+        const token = generateToken(newUser.id, organization.id);
+        
+        res.status(200).json({
+            status: "success",
+            data: {
+                token
+            }
+        });
+    } catch (error) {
+      console.log(error);
+    }
 });
 
 
 
 
-// exports.login = catchAsynch(async (req, res) => {
+exports.login = catchAsynch(async (req, res,next) => {
 
-// });
+    const { email, password } = req.body;
+    
+    if (!email || !password) {
+        return next(new AppError("please provide email and password", 400));
+    }
+
+    const user = await UserAccount.findOne({ email });
+
+    if (!user) return next(new AppError("invalid emial or password",401));
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        return next(new AppError("email or password is not correct please try again",401))
+    }
+
+    res.status(200).json({
+        status: "success",
+        token: generateToken(user.id, user.organizationId)
+    })
+
+
+});
--- models/userAccount.js
+++ models/userAccount.js
@@ -7,14 +7,12 @@ const UserAccount = sequelize.define('UserAccount', {
   username: { type: DataTypes.STRING(64), allowNull: false },
   password: { type: DataTypes.STRING(200), allowNull: false },
   email: { type: DataTypes.STRING(64), unique: true },
-  usertypeId: { type: DataTypes.INTEGER, allowNull: false },
+  usertypeId: {type: DataTypes.INTEGER, allowNull:false},
   whatsApp: DataTypes.STRING(64)
 }, {
   tableName: 'userAccounts',
   timestamps: false
 });
-
-
 
 UserAccount.belongsTo(Employee, { foreignKey: 'employeeId' });
 Employee.hasOne(UserAccount, { foreignKey: 'employeeId' });
