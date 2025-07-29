b4b6fc7c39e8a6d886dfc44914a5aa403be03915
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2025-07-29 23:15:21 +0430

    created protected middleware

---
+++ middlewares/authMiddlewares.js
@@ -0,0 +1,33 @@
+const jwt= require("jsonwebtoken");
+const AppError = require("../utils/appError");
+const catchAsynch = require("./catchAsynch");
+const UserAccount = require("../models/userAccount");
+
+
+exports.protect = catchAsynch(async (req, res,next) => {
+   
+
+    let token;
+
+    if (req.headers.authorization && 
+         req.headers.authorization.startsWith("Bearer")
+    ) {
+        token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+        return next(new AppError("you are not access to this page or data", 401));
+    }
+
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    
+    const user = await UserAccount.findByPk(decoded.id);
+    if (!user) {
+        return next(new AppError("user not found", 401));
+    }
+
+    
+
+
+});
