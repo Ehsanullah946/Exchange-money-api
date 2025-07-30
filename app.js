45e9c045d05d63295ee62e276592d32b2925ca1d
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2025-07-30 16:02:28 +0430

    adding route to app.js

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const express = require("express");
 const app = express();
 const authRouter = require("./routes/authRoutes");
 const customerRouter = require("./routes/customerRoute");
+const employeeRouter = require("./routes/employeeRoute");
+const exchangerRouter = require("./routes/exhangerRoute");
+const senderRceiverRouter = require("./routes/customerRoute");
 
 app.use(express.json());
 
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/customer", customerRouter);
+app.use("/api/v1/employee", employeeRouter);
+app.use("/api/v1/exchanger", exchangerRouter);
+app.use("/api/v1/senderReceiver", senderRceiverRouter);
 
 
 
