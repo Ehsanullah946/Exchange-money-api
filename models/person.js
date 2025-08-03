c403994adf56ef5276df98602f0f711e98be1537
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2025-08-03 23:17:41 +0430

    fixing some essues

--- models/person.js
+++ models/person.js
@@ -1,11 +1,23 @@
 module.exports = (sequelize, DataTypes) => {
   const Person = sequelize.define("Person", {
-    firstName: { type: DataTypes.STRING(32), allowNull: false },
-    lastName: { type: DataTypes.STRING(32) },
-    fatherName: { type: DataTypes.STRING(32) },
-    nationalCode: { type: DataTypes.STRING(64), unique: true },
-    phoneNo: { type: DataTypes.STRING(15) },
-    currentAddress: { type: DataTypes.TEXT }
+     id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+        firstName: {
+        type: DataTypes.STRING(32),
+        allowNull: false,     
+        },
+        lastName: DataTypes.STRING(32),
+        fatherName: DataTypes.STRING(32),
+        photo:DataTypes.STRING(300),
+        nationalCode: {
+            type: DataTypes.STRING(64),
+            unique: "person_nationalCode"
+        },
+        phoneNo: DataTypes.STRING(15),
+        currentAddress: DataTypes.TEXT,
+        organizationId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+       },
   },  {
     tableName: "persons",
     timestamps: false
