ee617135c5a9260ce5200ccf6d5990725e82d8e8
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2025-08-06 22:07:02 +0430

    created transfer delete operation

--- routes/transferRoute.js
+++ routes/transferRoute.js
@@ -9,8 +9,8 @@ const router = express.Router();
 
 // router.get('/', protect, allowRoles(2, 3, 4), orgScope(Transfer), transferController.getTransfers);
 router.post('/', protect, allowRoles(2, 3), orgScope(Transfer), transferController.createTransfer);
-// router.patch('/:id', protect, allowRoles(2, 3), orgScope(Transfer), transferController.updateTransfer);
-// router.delete('/:id', protect, allowRoles(2, 3), orgScope(Transfer), transferController.deleteTransfer);
+router.patch('/:id', protect, allowRoles(2, 3), orgScope(Transfer), transferController.updateTransfer);
+router.delete('/:id', protect, allowRoles(2, 3), orgScope(Transfer), transferController.deleteTransfer);
 // router.get('/:id', protect, allowRoles(2, 3), orgScope(Transfer), transferController.getTransferByid);
 
 module.exports = router;
