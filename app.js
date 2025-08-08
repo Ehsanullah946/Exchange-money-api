b40a38d24b2aa6d14100b9306dedc460eaf24034
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2025-08-08 20:51:34 +0430

    created receive rotues

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const branchRouter = require("./routes/branchRoute");
 const moneyType = require("./routes/moneyTypeRoute");
 const accountRouter = require("./routes/accountRoutes");
 const transferRouter = require("./routes/transferRoute");
+const receiveRouter = require("./routes/receiveRoutes");
 
 app.use(express.json());
 
@@ -24,6 +25,7 @@ app.use("/api/v1/branch", branchRouter);
 app.use("/api/v1/moneyType", moneyType);
 app.use("/api/v1/account", accountRouter);
 app.use("/api/v1/transfer", transferRouter);
+app.use("/api/v1/receive", receiveRouter);
 
 
 module.exports = app;
