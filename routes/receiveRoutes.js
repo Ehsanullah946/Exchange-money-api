518e997356e9dd0a58bf12dcbebb7b38109ad0ad
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2025-08-08 20:51:34 +0430

    created receive rotues

---
+++ routes/receiveRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { protect } = require('../middlewares/authMiddlewares');
+const orgScope = require('../middlewares/orgScope');
+const receiveController = require('../controllers/receive');
+const { Receive } = require('../models');
+const { allowRoles } = require('../middlewares/roleMiddleware');
+
+const router = express.Router();
+
+// router.get('/', protect, allowRoles(2, 3, 4), orgScope(Receive), receiveController.getReceives);
+router.post('/', protect, allowRoles(2, 3), orgScope(Receive), receiveController.createReceive);
+// router.put('/:id', protect, allowRoles(2, 3), orgScope(Receive), receiveController.updateReceive);
+// router.delete('/:id', protect, allowRoles(2, 3), orgScope(Receive), receiveController.deleteReceive);
+// router.patch('/:id/reject', protect, allowRoles(2, 3), orgScope(Receive), receiveController.rejectReceive);
+// router.get('/:id', protect, allowRoles(2, 3), orgScope(Receive), receiveController.getReceiveByid);
+
+module.exports = router;
