ada97db36183435d4c40a9772dc1fcf187bd4048
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2025-08-08 19:02:02 +0430

    created receive controller

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -7,7 +7,7 @@ const { Account } = require('../models');
 
 const router = express.Router();
 
-router.get('/', protect, allowRoles(2, 3, 4), orgScope(Account), accountController.getAccounts);
+router.get('/', protect, allowRoles(2,3,4), orgScope(Account), accountController.getAccounts);
 router.get('/:id', protect, allowRoles(2, 3, 4), orgScope(Account), accountController.getAccountById);
 router.post('/', protect, allowRoles(2, 3), orgScope(Account), accountController.createAccount);
 router.patch('/:id', protect, allowRoles(2, 3), orgScope(Account), accountController.updateAccount);
