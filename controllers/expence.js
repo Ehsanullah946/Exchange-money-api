407d2629e6feee97574d27b2c8de9f4bb8f2904d
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2025-08-16 17:12:40 +0430

    created expence soft delete operatio

--- controllers/expence.js
+++ controllers/expence.js
@@ -58,3 +58,32 @@ exports.updateExpence = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+exports.deleteExpence = async (req, res) => {
+  const t = await Expence.sequelize.transaction();
+  try {
+    const expence = await Expence.findOne(
+      {
+        ...req.orgQuery,
+        where: { ...req.orgQuery.where, No: req.params.id },
+      },
+      { transaction: t }
+    );
+
+    if (!expence) {
+      await t.rollback();
+      res.status(404).json('expence not found');
+    }
+
+    await Expence.update(
+      { deleted: true },
+      { where: { ...req.orgQuery.where, No: req.params.id }, transaction: t }
+    );
+
+    await t.commit();
+
+    res.status(200).json({ message: 'expence soft deleted' });
+  } catch (err) {
+    await t.rollback();
+    res.status(500).json({ message: err.message });
+  }
+};
